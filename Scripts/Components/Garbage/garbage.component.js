0b2287e6dfb0503634d81e69281d6556063b5b5c
Ninib Özmen <Ninib zmen>
2016-10-17 15:37:38 +0200

    jquery UI

---
+++ Scripts/Components/Garbage/garbage.component.js
@@ -0,0 +1,27 @@
+﻿
+angular.module("mainModule")
+    .component("garbage", {
+        templateUrl: "Scripts/Components/Garbage/Garbage.html",
+        controller: function () {
+            var scope = this;
+            var component = {};
+
+            setTimeout(function () {
+                component = $("#garbage-" + scope.id);
+                component.droppable({
+                    drop: function (event, ui) {
+                        var element = ui.draggable.css('position', '');
+                        $(this).append(element);
+
+                        $(ui.draggable).fadeOut(1000);
+                    }
+
+                });
+            });
+        },
+        controllerAs: "scope",
+        bindings: {
+            id: "="
+            
+        }
+    })
